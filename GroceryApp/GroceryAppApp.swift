0d2f2a51a3560fea484f69c3796725d63f1f4b1a
elizeurs <13ders@gmail.com>
2021-03-02 14:49:17 -0300

    Initial Commit

---
+++ GroceryApp/GroceryAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroceryAppApp.swift
+//  GroceryApp
+//
+//  Created by Elizeu RS on 02/03/21.
+//
+
+import SwiftUI
+
+@main
+struct GroceryAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
