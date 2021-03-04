57719a8e8f3a7e45c39ee9f64a30f481677cfaef
elizeurs <13ders@gmail.com>
2021-03-04 15:01:22 -0300

    Implementing FirestoreManager getAllStores

--- GroceryApp/Assets.xcassets/FirestoreManager.swift
+++ GroceryApp/Assets.xcassets/FirestoreManager.swift
@@ -17,22 +17,44 @@ class FirestoreManager {
     db = Firestore.firestore()
   }
   
-  func save(store: Store, completion: @escaping (Result<Store?, Error>) -> Void) {
-      
-      do {
-        let ref = try db.collection("stores").addDocument(from: store)
-          ref.getDocument { (snapshot, error) in
-              guard let snapshot = snapshot, error == nil else {
-                  completion(.failure(error!))
-                  return
-              }
-              
-              let store = try? snapshot.data(as: Store.self)
-              completion(.success(store))
+  func getAllStores(completion: @escaping (Result<[Store]?, Error>) -> Void) {
+    
+    db.collection("stores")
+      .getDocuments { (snapshot, error) in
+        if let error = error {
+          completion(.failure(error))
+        } else {
+          if let snapshot = snapshot {
+            let stores: [Store]? = snapshot.documents.compactMap { doc in
+            var store = try? doc.data(as: Store.self)
+            if store != nil {
+              store!.id = doc.documentID
+            }
+            return store
           }
           
-      } catch let error {
-          completion(.failure(error))
+          completion(.success(stores))
+        }
       }
   }
+}
+
+func save(store: Store, completion: @escaping (Result<Store?, Error>) -> Void) {
+  
+  do {
+    let ref = try db.collection("stores").addDocument(from: store)
+    ref.getDocument { (snapshot, error) in
+      guard let snapshot = snapshot, error == nil else {
+        completion(.failure(error!))
+        return
+      }
+      
+      let store = try? snapshot.data(as: Store.self)
+      completion(.success(store))
+    }
+    
+  } catch let error {
+    completion(.failure(error))
+  }
+}
 }
